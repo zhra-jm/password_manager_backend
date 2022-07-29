1cb6e194f2e2cd227410c6d107f39c525183c3e8
Your Name <zahrajam1999@gmail.com>
2022-07-30 01:23:21 +0430

    models added

---
+++ source/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, Boolean, String, ForeignKey
+from .database import Base
+
+
+class MasterPassword(Base):
+    __tablename__ = "master_password"
+
+    id = Column(Integer, primary_key=True, index=True)
+    master_password = Column(String)
+    hint = Column(String)
+
+
+class Password(Base):
+    __tablename__ = "password"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, primary_key=True, index=True)
+    username = Column(String)
+    password = Column(String)
+    url = Column(String)
